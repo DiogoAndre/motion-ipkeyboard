3e9265713b13ef38e6365f8a056bb8a5bf541c5f
diogoandre <diogo@regattapix.com>
2014-08-12 14:00:54 -0300

    Improving layout
    improving how the keyboard layout is done.
    also added some accessibility labels to the demo app

--- app/ipkeyboard_controller.rb
+++ app/ipkeyboard_controller.rb
@@ -8,11 +8,13 @@ class IPKeyboardController < UIViewController
     ipv6_input_field = UITextField.alloc.initWithFrame(CGRectMake(10, 100, 300, 45))
     ipv6_input_field.setBackgroundColor(UIColor.whiteColor)
     ipv6_input_field.inputView = RMIPKeyboard::Keyboard.alloc.initWithTextFieldAndLayout(ipv6_input_field,"ipv6")
+    ipv6_input_field.setAccessibilityLabel("IPv6")
     ipv6_input_field.setDelegate(self)
 
     ipv4_input_field = UITextField.alloc.initWithFrame(CGRectMake(10, 50, 300, 45))
     ipv4_input_field.setBackgroundColor(UIColor.whiteColor)
     ipv4_input_field.inputView = RMIPKeyboard::Keyboard.alloc.initWithTextFieldAndLayout(ipv4_input_field,"ipv4")
+    ipv4_input_field.setAccessibilityLabel("IPv4")
     ipv4_input_field.setDelegate(self)
 
     self.view.addSubview(ipv6_input_field)
--- lib/RMIPKeyboard/RMIPKeyboard.rb
+++ lib/RMIPKeyboard/RMIPKeyboard.rb
@@ -1,120 +1,117 @@
 module RMIPKeyboard
   class Keyboard < UIView
-    HEX_KEYBOARD_HEIGHT = 305
-    DEC_KEYBOARD_HEIGHT = 205
-    SGRAYCOLOUR = UIColor.lightTextColor
 
     def initWithTextFieldAndLayout(textField, klayout="ipv6")
-      if klayout == "ipv6"
-        @keyboardHeight = HEX_KEYBOARD_HEIGHT
-        @numrows = 6.0
-        @dividerChar = ":"
-        @numberOfKeys = 15 # minus one
-      else
-        @keyboardHeight = DEC_KEYBOARD_HEIGHT
-        @numrows = 4.0
-        @dividerChar = "."
-        @numberOfKeys = 9 # minus one
-      end
-
-      self.initWithFrame(CGRectMake(0.0, 0.0, 320.0, @keyboardHeight))
+      
+      define_constants
+      calc_sizes(klayout)
+      
+      self.initWithFrame(CGRectMake(0.0, 0.0, @screen_width, @keyboardHeight))
 
       @textField = WeakRef.new(textField)
-      self.backgroundColor = UIColor.lightGrayColor
+      self.backgroundColor = @bg_color
       self.createButtons
-
       self
+    end
+    
+    def define_constants
+      @bg_color = UIColor.lightGrayColor
+      @text_color = UIColor.lightTextColor
+      @screen_width = UIScreen.mainScreen.bounds.size.width
+      @screen_height = UIScreen.mainScreen.bounds.size.height
+    end
+    
+    def calc_sizes(klayout)
+      
+      if klayout == "ipv6"
+        @key_height = 49
+        @numrows = 6.0
+        @dividerChar = ":"
+        @numberOfKeys = 18
+      else
+        @key_height = 54
+        @numrows = 4.0
+        @dividerChar = "."
+        @numberOfKeys = 12
+        @first_origin        
+      end
+      
+      @keyboardHeight = @key_height * @numrows
+      @key_width  = @screen_width / 3.0
+      @rect = CGRectMake(0.0, 0.0, @key_width, @key_height)    
     end
 
     def createButtons
-      rect = CGRectMake(0.0, 0.0, ((self.bounds.size.width / 3.0) + 0.3).floor, (((@keyboardHeight - 5.0) / @numrows) + 0.3))
-
-      # Makes the numerical buttons
       (1..@numberOfKeys).each do |num|
-        rect.origin = self.buttonOriginPointForNumber(num)
-
-        self.makeButtonWithRect(rect,self.buttonTitleForNumber(num),false)
+        @rect.origin = self.buttonOriginPointForNumber(num)
+        self.makeButtonWithRect(@rect,num,false)
       end
-      # Makes the ':' button
-      rect.origin = self.buttonOriginPointForNumber(@numberOfKeys + 1)
-
-      self.makeButtonWithRect(rect,@dividerChar,true)
-
-      # Makes the '0' button
-      rect.origin = self.buttonOriginPointForNumber(@numberOfKeys + 2)
-
-      self.makeButtonWithRect(rect,"0",false)
-
-      # Makes the 'delete' button
-      rect.origin = self.buttonOriginPointForNumber(@numberOfKeys + 3)
-
-      button = UIButton.alloc.initWithFrame(rect)
-      button.tag = @numberOfKeys + 3
-      button.backgroundColor = SGRAYCOLOUR
-      button.setImage(UIImage.imageNamed("deleteButton"), forState:UIControlStateNormal)
-      button.addTarget(self, action:"changeButtonBackgroundColourForHighlight:", forControlEvents:(UIControlEventTouchDown|UIControlEventTouchDragEnter|UIControlEventTouchDragExit))
-      button.addTarget(self, action:"changeTextFieldText:", forControlEvents:UIControlEventTouchUpInside)
-
-      self.addSubview(button)
     end
 
-    def makeButtonWithRect(rect, title, grayBackground)
+    def makeButtonWithRect(rect, num, grayBackground)
       button = UIButton.buttonWithType(UIButtonTypeCustom)
       button.frame = rect
       fontSize = 25.0
 
-      unless  NSCharacterSet.decimalDigitCharacterSet.isSupersetOfSet(NSCharacterSet.characterSetWithCharactersInString(title))
-        fontSize = 20.0
+      button.backgroundColor = grayBackground ? @text_color : UIColor.whiteColor
+      
+      if num != @numberOfKeys
+        button.titleLabel.font = UIFont.systemFontOfSize(fontSize)
+        button.setTitleColor(UIColor.darkTextColor, forState:UIControlStateNormal)
+        button.setTitle(buttonTitleForNumber(num), forState:UIControlStateNormal)
+      else
+        #button.backgroundColor = @text_color
+        button.setImage(UIImage.imageNamed("deleteButton"), forState:UIControlStateNormal)
+        button.setAccessibilityLabel("delete")
       end
-
-      button.backgroundColor = grayBackground ? SGRAYCOLOUR : UIColor.whiteColor
-      #button.backgroundColor = grayBackground ? UIColor.lightTextColor : UIColor.whiteColor
-      button.titleLabel.font = UIFont.systemFontOfSize(fontSize)
-      button.setTitleColor(UIColor.darkTextColor, forState:UIControlStateNormal)
-      button.setTitle(title, forState:UIControlStateNormal)
+      
+      if num == @numberOfKeys || num == (@numberOfKeys - 2)
+        button.backgroundColor = @text_color
+      end
+      
+      button.tag = num
       button.addTarget(self, action:"changeButtonBackgroundColourForHighlight:", forControlEvents: (UIControlEventTouchDown|UIControlEventTouchDragEnter|UIControlEventTouchDragExit))
       button.addTarget(self, action:"changeTextFieldText:", forControlEvents: UIControlEventTouchUpInside)
-
+      
       self.addSubview(button)
     end
 
     def buttonTitleForNumber(num)
       str = num.to_s
-      if num <= 15
-        if num >= 10
-          str = ["A", "B", "C", "D", "E", "F"][num - 10]
-        end
-      else
-        str = "F#%K"
+      
+      if num > 9 && @numberOfKeys == 18
+        str = ["a", "b", "c", "d", "e", "f",@dividerChar,"0","del"][num - 10]
+      elsif num > 9
+        str = [@dividerChar,"0","del"][num - 10]
       end
       str
     end
-
+    
     def buttonOriginPointForNumber(num)
       point = CGPointMake(0.0,0.0)
-
+    
       if (num % 3) == 2 # 2nd button in the row
-        point.x = (self.bounds.size.width / 3.0).ceil
+        point.x = @key_width.ceil
       elsif (num % 3) == 0 # 3rd button in the row
-        point.x = (self.bounds.size.width / 3.0 * 2.0).ceil
+        point.x = (@key_width * 2.0).ceil
       end
-
+    
       if num > 3 # The row multiplied by row's height
-        point.y = ((num - 1) / 3.0).floor * (@keyboardHeight / @numrows)
+        point.y = ((num - 1) / 3.0).floor * (@key_height + 0.5)
       end
       point
     end
 
     def changeButtonBackgroundColourForHighlight(button)
-      if button.backgroundColor == SGRAYCOLOUR
+      if button.backgroundColor == @text_color
          button.backgroundColor = UIColor.whiteColor
       else
-         button.backgroundColor = SGRAYCOLOUR
+         button.backgroundColor = @text_color
       end
     end
 
     def changeTextFieldText(button)
-      if button.tag == @numberOfKeys + 3
+      if button.tag == @numberOfKeys # Last key
         @textField.text = "#{@textField.text.chop}"
       else
         @textField.text = "#{@textField.text}#{button.titleLabel.text}"
